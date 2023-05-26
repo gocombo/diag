0065cd6fe942daf0a553978ff366aca50c5b0009
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2023-05-26 11:31:16 +0200

    Log levels.

--- go.mod
+++ go.mod
@@ -4,11 +4,16 @@ go 1.20
 
 require (
 	github.com/gofrs/uuid v4.4.0+incompatible
+	github.com/jaswdr/faker v1.17.0
 	github.com/rs/zerolog v1.29.1
+	github.com/stretchr/testify v1.8.3
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ logger_test.go
@@ -0,0 +1,88 @@
+package diag
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"testing"
+
+	"github.com/gofrs/uuid"
+	"github.com/jaswdr/faker"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestContext used for testing purposes only
+type TestContext struct {
+	CorrelationID string `json:"correlationId"`
+}
+
+// TestLogMessage used for testing purposes only
+type TestLogMessage struct {
+	Level   string      `json:"level"`
+	Msg     string      `json:"msg"`
+	Time    string      `json:"time"`
+	Context TestContext `json:"context"`
+}
+
+func TestLoggerLevel(t *testing.T) {
+	fake := faker.New()
+
+	var output bytes.Buffer
+	outputWriter := bufio.NewWriter(&output)
+
+	ctx := RootContext(
+		NewRootContextParams().
+			WithOutput(outputWriter).
+			WithCorrelationID(uuid.Must(uuid.NewV4()).String()),
+	)
+	log := Log(ctx)
+
+	type testCase struct {
+		log       LogLevelEvent
+		wantLevel string
+	}
+
+	tests := []testCase{
+		{
+			log:       log.Error(),
+			wantLevel: "error",
+		},
+		{
+			log:       log.Warn(),
+			wantLevel: "warn",
+		},
+		{
+			log:       log.Info(),
+			wantLevel: "info",
+		},
+		{
+			log:       log.Debug(),
+			wantLevel: "debug",
+		},
+		{
+			log:       log.Trace(),
+			wantLevel: "trace",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.wantLevel, func(t *testing.T) {
+			output.Reset()
+			msg := fake.Lorem().Sentence(3)
+			tt.log.Msg(msg)
+			outputWriter.Flush()
+
+			var logMessage TestLogMessage
+			assert.NoError(t, json.Unmarshal(output.Bytes(), &logMessage))
+
+			assert.Equal(t, TestLogMessage{
+				Level: tt.wantLevel,
+				Msg:   msg,
+				Time:  logMessage.Time,
+				Context: TestContext{
+					CorrelationID: logMessage.Context.CorrelationID,
+				},
+			}, logMessage)
+		})
+	}
+}
