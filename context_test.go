3ffd2b2cc33a5fa9324dfc6c51e1757f7b313b39
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2023-05-28 20:40:56 +0200

    More tests.

--- context_test.go
+++ context_test.go
@@ -3,10 +3,13 @@ package diag
 import (
 	"testing"
 
+	"github.com/jaswdr/faker"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestRootContext(t *testing.T) {
+	fake := faker.New()
+
 	t.Run("initializes a new diag context", func(t *testing.T) {
 		ctx := RootContext(NewRootContextParams())
 		log := Log(ctx)
@@ -14,5 +17,16 @@ func TestRootContext(t *testing.T) {
 		diagData := DiagData(ctx)
 		assert.NotEmpty(t, diagData.CorrelationID)
 		assert.Empty(t, diagData.Entries)
+	})
+	t.Run("initializes a new diag context with custom data", func(t *testing.T) {
+		wantCorrelationID := fake.UUID().V4()
+		ctx := RootContext(
+			NewRootContextParams().
+				WithCorrelationID(wantCorrelationID),
+		)
+		log := Log(ctx)
+		assert.NotNil(t, log)
+		diagData := DiagData(ctx)
+		assert.Equal(t, wantCorrelationID, diagData.CorrelationID)
 	})
 }
