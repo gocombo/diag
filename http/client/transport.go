d210a6c60d53f49920dae78c30629988d3dd4fce
Evgeny Myasishchev <evgenymyasishchev@gmail.com>
2024-04-18 12:11:10 +0200

    http client transport: allow obfuscating additional headers as option

--- http/client/transport.go
+++ http/client/transport.go
@@ -20,6 +20,7 @@ func writeLogEndMessage(
 	durationSec float64,
 	req *http.Request,
 	res *http.Response,
+	obfuscateHeaders []string,
 ) {
 	var levelLog diag.LogLevelEvent
 	var resCode int
@@ -40,7 +41,7 @@ func writeLogEndMessage(
 		Float64("durationSec", durationSec).
 		Int("statusCode", resCode)
 	if res != nil {
-		logData = logData.Interface("headers", internal.FlattenAndObfuscate(res.Header, internal.DefaultObfuscatedHeaders))
+		logData = logData.Interface("headers", internal.FlattenAndObfuscate(res.Header, obfuscateHeaders))
 	}
 
 	levelLog.
@@ -48,22 +49,46 @@ func writeLogEndMessage(
 		Msgf("COMPLETE SENDING REQ: %d - %v", resCode, req.URL.String())
 }
 
+type transportCfg struct {
+	obfuscateHeaders []string
+}
+
+// TransportOption is a functional option for configuring the transport
+type TransportOption func(*transportCfg)
+
+// WithObfuscateHeaders sets the headers that should be obfuscated in the logs
+func WithObfuscateHeaders(headers []string) TransportOption {
+	return func(cfg *transportCfg) {
+		lowercaseHeaders := make([]string, len(headers))
+		for i, header := range headers {
+			lowercaseHeaders[i] = strings.ToLower(header)
+		}
+		cfg.obfuscateHeaders = append(cfg.obfuscateHeaders, lowercaseHeaders...)
+	}
+}
+
 // NewTransport returns a wrapped http.RoundTripper that will produce
 // diag like logs for each request
 // TODO: Unit test this
-func NewTransport(target http.RoundTripper) http.RoundTripper {
+func NewTransport(target http.RoundTripper, opts ...TransportOption) http.RoundTripper {
+	cfg := &transportCfg{
+		obfuscateHeaders: internal.DefaultObfuscatedHeaders,
+	}
+	for _, opt := range opts {
+		opt(cfg)
+	}
 	return roundTripperFn(func(req *http.Request) (*http.Response, error) {
 		log := diag.Log(req.Context())
 		log.Info().WithData(
 			log.NewData().
-				Interface("headers", internal.FlattenAndObfuscate(req.Header, internal.DefaultObfuscatedHeaders)).
+				Interface("headers", internal.FlattenAndObfuscate(req.Header, cfg.obfuscateHeaders)).
 				Str("method", req.Method).
 				Str("url", req.URL.String()),
 		).Msgf("START SENDING REQ: %s %s", strings.ToUpper(req.Method), req.URL)
 		startedAt := time.Now()
 		res, err := target.RoundTrip(req)
 		reqDuration := time.Since(startedAt).Seconds()
-		writeLogEndMessage(log, reqDuration, req, res)
+		writeLogEndMessage(log, reqDuration, req, res, cfg.obfuscateHeaders)
 		return res, err
 	})
 }
